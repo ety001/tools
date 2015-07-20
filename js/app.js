fe584a6047e68fcd8cab6f587372ac3710fd6ddb
ETY001 <ety001@domyself.me>
2015-07-21 00:09:30 +0800

    beata1

---
+++ js/app.js
@@ -0,0 +1,31 @@
+/*
+**** PRODUCT LIST ****
+1. Upan
+2. Online Clipboard
+3. Review Bookmark
+4. Pix
+5. SS
+6. Ji Zhang
+7. Party Time
+
+**** DONATE ME ****
+Type 'donate' to donate me.
+
+**** MY BLOG ****
+Type 'blog' to enter my blog.
+*/
+var app = {
+  list: {
+    1: 'http://upan.pro',
+    2: 'http://oc.to0l.cn',
+    3: 'http://bm.to0l.cn',
+    3: 'http://pix.domyself.me',
+  }
+  run: function(){
+
+  }
+};
+
+$(function(){
+  app.run();
+})
